ad91b868c6f297a2a05ef28cea0cb7350ec7122b
Tran Tuan <tran.van.tuan@mqsolutions.com.vn>
2020-04-01 16:19:02 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      port: 3001,
+      retryAttempts: 5,
+      retryDelay: 3000,
+    },
+  });
+  await app.startAllMicroservicesAsync();
   await app.listen(3000);
 }
 bootstrap();
